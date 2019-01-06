09fef2da40043d17c77c4d129a33ebdbe52d9024
Osiozekhai Aliu <aliu@dev-hh.de>
2019-01-07 00:18:00 +0100

    removed smile/elasticsuite

--- .docker/php/scripts/install-magento.sh
+++ .docker/php/scripts/install-magento.sh
@@ -5,7 +5,8 @@ if true = $1 ; then
     su -c "composer global require hirak/prestissimo;" -s /bin/sh $2
     su -c "composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=2.2.7 ." -s /bin/sh $2
 
-    su -c "composer require smile/elasticsuite:2.6;" -s /bin/sh $2
+    #  su -c "bin/magento sampledata:deploy" -s /bin/sh $2
+    # su -c "composer require smile/elasticsuite:2.6;" -s /bin/sh $2
 
     su -c "composer require predis/predis \
                 --dev msp/devtools \

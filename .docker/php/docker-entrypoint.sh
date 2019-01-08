c82fa1c36abe440262adf477c422850e27c3a61d
Osiozekhai Aliu <aliu@dev-hh.de>
2019-01-08 20:41:42 +0100

    removed mhsendmail override of sendmail
    removed go and git packages

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -3,8 +3,9 @@
 if [[ $1 = "true" ]]
 then
     cd $3;
-    su -c "composer global require hirak/prestissimo;" -s /bin/sh $2
+    su -c "composer global require hirak/prestissimo" -s /bin/sh $2
     su -c "composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=$4 ." -s /bin/sh $2
+    su -c "composer require mageplaza/module-smtp" -s /bin/sh $2
 
     # su -c "composer require predis/predis \
     #   --dev msp/devtools \

bbc2eb7f10a11009950d9d644fcdb79607fd9388
Osiozekhai Aliu <aliu@dev-hh.de>
2019-01-08 09:45:00 +0100

    removed extra magento 2 composer packages

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -6,11 +6,12 @@ then
     su -c "composer global require hirak/prestissimo;" -s /bin/sh $2
     su -c "composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=$4 ." -s /bin/sh $2
 
-    su -c "composer require predis/predis \
-            --dev msp/devtools \
-            --dev mage2tv/magento-cache-clean \
-            splendidinternet/mage2-locale-de-de;" -s /bin/sh $2
+    # su -c "composer require predis/predis \
+    #   --dev msp/devtools \
+    #   --dev mage2tv/magento-cache-clean \
+    #   splendidinternet/mage2-locale-de-de;" -s /bin/sh $2
 
-    su -c "composer config repositories.firegento_magesetup vcs git@github.com:firegento/firegento-magesetup2.git; \
-           composer require firegento/magesetup2:dev-develop;" -s /bin/sh $2
+    # su -c "composer config repositories.firegento_magesetup vcs git@github.com:firegento/firegento-magesetup2.git; \
+    #   composer require firegento/magesetup2:dev-develop;" -s /bin/sh $2
+
 fi

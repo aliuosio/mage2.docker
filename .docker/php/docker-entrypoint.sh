32cf2b93d2c0b424400c75bd4c6ee6d25b2d5d5b
Osiozekhai Aliu <aliu@dev-hh.de>
2019-01-09 10:46:15 +0100

    added flag to install sample data

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -5,7 +5,9 @@ then
     cd $3;
     su -c "composer global require hirak/prestissimo" -s /bin/sh $2
     su -c "composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=$4 ." -s /bin/sh $2
-    su -c "ln -s ../.composer/auth.json $3/var/composer_home/auth.json" -s /bin/sh $2
+    su -c "mkdir -p var/composer_home" -s /bin/sh $2
+    su -c "chown -R $2:$2 ../" -s /bin/sh $2
+    su -c "cp ../.composer/auth.json $3/var/composer_home/auth.json" -s /bin/sh $2
     su -c "composer require mageplaza/module-smtp" -s /bin/sh $2
 
     su -c "composer require predis/predis \
@@ -16,5 +18,8 @@ then
     su -c "composer config repositories.firegento_magesetup vcs git@github.com:firegento/firegento-magesetup2.git; \
        composer require firegento/magesetup2:dev-develop;" -s /bin/sh $2
 
-    su -c "bin/magento sampledata:deploy;" -s /bin/sh $2
+    if [[ $5 = "true" ]]
+        su -c "bin/magento sampledata:deploy;" -s /bin/sh $2
+    fi
+
 fi

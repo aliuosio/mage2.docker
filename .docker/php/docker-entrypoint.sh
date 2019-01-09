42e0e4e354d4f0a9ce6b195e3fe4aaa2f15bba54
Osiozekhai Aliu <aliu@dev-hh.de>
2019-01-09 08:51:03 +0100

    added extra composer packages

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -8,13 +8,13 @@ then
     su -c "ln -s $3/../.composer/auth.json $3/var/composer_home/auth.json" -s /bin/sh $2
     su -c "composer require mageplaza/module-smtp" -s /bin/sh $2
 
-    # su -c "composer require predis/predis \
-    #    --dev msp/devtools \
-    #    --dev mage2tv/magento-cache-clean \
-    #    splendidinternet/mage2-locale-de-de;" -s /bin/sh $2
+    su -c "composer require predis/predis \
+       --dev msp/devtools \
+       --dev mage2tv/magento-cache-clean \
+       splendidinternet/mage2-locale-de-de;" -s /bin/sh $2
 
-    # su -c "composer config repositories.firegento_magesetup vcs git@github.com:firegento/firegento-magesetup2.git; \
-    #    composer require firegento/magesetup2:dev-develop;" -s /bin/sh $2
+    su -c "composer config repositories.firegento_magesetup vcs git@github.com:firegento/firegento-magesetup2.git; \
+       composer require firegento/magesetup2:dev-develop;" -s /bin/sh $2
 
     su -c "bin/magento sampledata:deploy; \
         bin/magento setup:upgrade; \

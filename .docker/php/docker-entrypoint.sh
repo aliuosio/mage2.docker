a10d97f373d9784fab2f9fb09a6e9f2045178fd9
Osiozekhai Aliu <aliu@dev-hh.de>
2019-01-10 21:36:20 +0100

    sampledata is installed automatically

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -17,7 +17,6 @@ if [[ $1 = "true" ]]; then
     su -c "composer config repositories.firegento_magesetup vcs git@github.com:firegento/firegento-magesetup2.git; \
        composer require firegento/magesetup2:dev-develop;" -s /bin/sh $2
 
-    if [[ $5 = "true" ]]; then
-        su -c "bin/magento sampledata:deploy;" -s /bin/sh $2
-    fi
+    su -c "bin/magento sampledata:deploy;" -s /bin/sh $2
+
 fi

902cfc65e132ceede5112d3a479f1fbf8d3ef1b8
Osiozekhai Aliu <aliu@dev-hh.de>
2019-01-18 11:59:51 +0100

    added magento sample data install flag

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -18,5 +18,8 @@ if [[ $1 = "true" ]]; then
     #su -c "composer config repositories.firegento_magesetup vcs git@github.com:firegento/firegento-magesetup2.git; \
     #   composer require firegento/magesetup2:dev-develop;" -s /bin/sh $2
 
-    su -c "bin/magento sampledata:deploy;" -s /bin/sh $2
+    if [[ $5 = "true" ]]; then
+        su -c "bin/magento sampledata:deploy;" -s /bin/sh $2
+    fi
+
 fi

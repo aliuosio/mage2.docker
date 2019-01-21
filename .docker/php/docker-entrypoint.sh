cee07f46f4e84ccd4aee8bc8db90a692274ba173
Osiozekhai Aliu <aliu@dev-hh.de>
2019-01-21 14:47:25 +0100

    refactored

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -8,12 +8,9 @@ if [[ $1 = "true" ]]; then
     su -c "mkdir -p var/composer_home" -s /bin/sh $2
     su -c "chown -R $2:$2 ../" -s /bin/sh $2
     su -c "cp ../.composer/auth.json $3/var/composer_home/auth.json" -s /bin/sh $2
-    su -c "composer require mageplaza/module-smtp" -s /bin/sh $2
+    su -c "composer require mageplaza/module-smtp splendidinternet/mage2-locale-de-de;" -s /bin/sh $2
 
-    su -c "composer require predis/predis \
-       --dev msp/devtools \
-       --dev mage2tv/magento-cache-clean \
-       splendidinternet/mage2-locale-de-de;" -s /bin/sh $2
+    su -c "composer require --dev msp/devtools --dev mage2tv/magento-cache-clean;" -s /bin/sh $2
 
     #su -c "composer config repositories.firegento_magesetup vcs git@github.com:firegento/firegento-magesetup2.git; \
     #   composer require firegento/magesetup2:dev-develop;" -s /bin/sh $2

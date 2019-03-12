3a50f4cdd132556dd1c462ed8533606f5c1ba590
Osiozekhai Aliu <oaliu@hotmail.com>
2019-03-12 09:55:27 +0100

    modified php settings

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -5,53 +5,19 @@ curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin
 && chmod +x /usr/local/bin/composer;
 
 # install magerun2
-curl -L https://files.magerun.net/n98-magerun2.phar > /usr/local/bin/n98-magerun2.phar;
-chmod +x /usr/local/bin/n98-magerun2.phar
+curl -L https://files.magerun.net/n98-magerun2.phar > /usr/local/bin/n98-magerun2.phar \
+&& chmod +x /usr/local/bin/n98-magerun2.phar
 
 # composer downloader package to increase download speeds
-su -c "composer global require hirak/prestissimo" -s /bin/sh $2
+su -c "composer global require hirak/prestissimo" -s /bin/sh $1
 
 # go to magento root folder
-cd $3;
+cd $2;
 
-# run composer update
-if [[ $10 = "true" ]]; then
-    su -c "composer update;" -s /bin/sh $2
-fi
-
-if [[ $1 = "true" ]]; then
-
-    # download magento
-    su -c "
-    composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=$4 .;
-    mkdir -p var/composer_home;
-    cp ../.composer/auth.json ./var/composer_home/auth.json;
-    composer require --dev msp/devtools --dev mage2tv/magento-cache-clean;
-    " -s /bin/sh $2
-
-    # firegento magesetup install
-    if [[ $6 = "true" ]]; then
-        su -c "
-        composer config repositories.firegento_magesetup vcs git@github.com:firegento/firegento-magesetup2.git;
-        composer require firegento/magesetup2:dev-develop;
-        " -s /bin/sh $2
-    fi
-
-    # Magento Sample Data
-    if [[ $5 = "true" ]]; then
-        su -c "bin/magento sampledata:deploy;" -s /bin/sh $2
-    fi
-
-    # set owner and user permissions on magento folders
-    su -c "
-    find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
-    find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
-    chmod u+x bin/magento
-    " -s /bin/sh $2
-fi
+su -c "composer update;" -s /bin/sh $1
 
 # Xdebug Install
-if [[ $8 = "true" ]]; then
+if [[ $3 = "true" ]]; then
     pecl install -o -f xdebug;
     docker-php-ext-enable xdebug;
     sed -i "s#xdebug.remote_enable= 0#xdebug.remote_enable=1#g" /usr/local/etc/php/conf.d/xdebug.ini;

2c4366ceeaa2ada99f593f60c6539cbd4a2c2002
Osiozekhai Aliu <oaliu@hotmail.com>
2019-03-12 17:08:53 +0100

    reduced containaers and volumes to necessary

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -9,12 +9,33 @@ curl -L https://files.magerun.net/n98-magerun2.phar > /usr/local/bin/n98-magerun
 && chmod +x /usr/local/bin/n98-magerun2.phar
 
 # composer downloader package to increase download speeds
-su -c "composer global require hirak/prestissimo" -s /bin/sh $1
+su -c "composer global require hirak/prestissimo" -s /bin/sh $2
 
-# go to magento root folder
-cd $2;
+if [[ $1 = "true" ]]; then
 
-su -c "composer update;" -s /bin/sh $1
+    # go to magento root folder
+    cd $3;
+
+    # download magento
+    su -c "
+    composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=$4 .;
+    mkdir -p var/composer_home;
+    cp ../.composer/auth.json ./var/composer_home/auth.json;
+    composer require --dev msp/devtools --dev mage2tv/magento-cache-clean;
+    " -s /bin/sh $2
+
+    # Magento Sample Data
+    if [[ $5 = "true" ]]; then
+        su -c "bin/magento sampledata:deploy;" -s /bin/sh $2
+    fi
+
+    # set owner and user permissions on magento folders
+    su -c "
+    find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
+    find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
+    chmod u+x bin/magento
+    " -s /bin/sh $2
+fi
 
 # Xdebug Install
 if [[ $3 = "true" ]]; then

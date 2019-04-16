84891b4e75dc3ac07d503715e744cd4b99fb1867
Osiozekhai Aliu <oaliu@hotmail.com>
2019-04-17 01:20:39 +0200

    fixed composer installer

--- .docker/php/post-build.sh
+++ .docker/php/post-build.sh
@@ -7,10 +7,12 @@ if [[ $7 = "true" ]]; then
 fi
 
 # install composer
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer \
-&& chmod +x /usr/local/bin/composer;
+echo 'Composer Install';
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer;
+chmod +x /usr/local/bin/composer;
 
 # install magerun2
+echo 'Magerun2 Install';
 curl -L https://files.magerun.net/n98-magerun2.phar > /usr/local/bin/n98-magerun2.phar \
 && chmod +x /usr/local/bin/n98-magerun2.phar
 
@@ -26,6 +28,7 @@ fi
 
 # download magento 2
 if [[ $1 = "true" ]]; then
+	echo 'Downloading Magento 2';
     su -c "
     composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=$4 .;
     mkdir -p var/composer_home;
@@ -34,12 +37,35 @@ if [[ $1 = "true" ]]; then
     " -s /bin/sh $2
 
     # set owner and user permissions on magento folders
+    echo 'Set permissions';
     su -c "find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
         find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
         chmod u+x bin/magento;" -s /bin/sh $2
+
+ <<'END'
+	su -c "bin/magento setup:install \
+	    --db-host=mysql \
+	    --db-name=$8 \
+	    --db-user=$9 \
+	    --db-password=$10 \
+	    --backend-frontname=admin \
+	    --base-url=https://mage2.doc/ \
+	    --language=de_DE \
+	    --timezone=Europe/Berlin \
+	    --currency=EUR \
+	    --admin-lastname=Admin \
+	    --admin-firstname=Admin \
+	    --admin-email=admin@example.com \
+	    --admin-user=admin \
+	    --admin-password=admin123 \
+	    --cleanup-database \
+	    --use-rewrites=1 \
+	    --use-sample-data" -s /bin/sh $2
+END
 fi
 
 # Magento Sample Data
 if [[ $5 = "true" ]]; then
+	echo 'Downloading Sample Data';
     su -c "bin/magento sampledata:deploy;" -s /bin/sh $2
 fi

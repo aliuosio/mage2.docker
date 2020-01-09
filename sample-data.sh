45401a5671c9d5cb3771efa7f40e34a251cdf7b1
aliuosio <oaliu@hotmail.com>
2020-01-09 17:55:49 +0100

    removed code compilation
    removed static content deploy

--- sample-data.sh
+++ sample-data.sh
@@ -3,12 +3,12 @@
 set -e
 
 install() {
-	docker exec -it -u $1 $2 bin/magento sampledata:deploy; 
-	docker exec -it -u $1 $2 bin/magento se:up; 
-	docker exec -it -u $1 $2 bin/magento se:di:co; 
-	docker exec -it -u $1 $2 bin/magento i:rei; 
-	docker exec -it -u $1 $2 bin/magento c:c; 
-	docker exec -it -u $1 $2 bin/magento setup:static-content:deploy -f de_DE en_US;
+	docker exec -it -u $1 $2 bin/magento sampledata:deploy;
+	docker exec -it -u $1 $2 bin/magento se:up;
+	# docker exec -it -u $1 $2 bin/magento se:di:co;
+	docker exec -it -u $1 $2 bin/magento i:rei;
+	docker exec -it -u $1 $2 bin/magento c:c;
+	# docker exec -it -u $1 $2 bin/magento setup:static-content:deploy -f de_DE en_US;
 }
 
 . ${PWD}/.env;

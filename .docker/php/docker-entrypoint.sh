d8cd21dff28e72915f22f84e0ff194df7967f55a
aliuosio <oaliu@hotmail.com>
2020-04-03 16:17:10 +0200

    moved xdebug config to entrypoint script

---
+++ .docker/php/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+# Set xdebug
+if [[ ${XDEBUG_ENABLE} = "true" ]]; then \
+    pecl install -o -f xdebug \
+    && docker-php-ext-enable xdebug \
+    && sed -i "s#xdebug.remote_enable=0#xdebug.remote_enable=1#g" /usr/local/etc/php/conf.d/xdebug.ini \
+    && sed -i "s#xdebug.remote_autostart=0#xdebug.remote_autostart=1#g" /usr/local/etc/php/conf.d/xdebug.ini; \
+else
+    pecl uninstall xdebug \
+    && sed -i "s#xdebug.remote_enable=1#xdebug.remote_enable=0#g" /usr/local/etc/php/conf.d/xdebug.ini \
+    && sed -i "s#xdebug.remote_autostart=1#xdebug.remote_autostart=0#g" /usr/local/etc/php/conf.d/xdebug.ini; \
+fi
+
+php-fpm -F
+
+exec "$@"

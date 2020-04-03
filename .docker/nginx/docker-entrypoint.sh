6d4286a1e5149b8e385e4a61b13f76ba40b6607b
aliuosio <oaliu@hotmail.com>
2020-04-03 14:11:50 +0200

    removed call to entrypoint.sh on php container

--- .docker/nginx/docker-entrypoint.sh
+++ .docker/nginx/docker-entrypoint.sh
@@ -6,7 +6,11 @@ set -e
 if [[ "$AUTH_CONFIG" = "true" ]]; then \
     printf "${AUTH_USER}:$(openssl passwd -crypt ${AUTH_PASS})\n" >> /etc/nginx/.htpasswd \
     && sed -i "s/# auth_basic/auth_basic/g" /etc/nginx/conf.d/default.conf \
-    && sed -i "s/# auth_basic/auth_basic/g" /etc/nginx/conf.d/default_ssl.conf; \
+    && sed -i "s/# auth_basic/auth_basic/g" /etc/nginx/conf.d/default_ssl.conf;
+else
+    rm /etc/nginx/.htpasswd \
+    && sed -i "s/auth_basic/# auth_basic/g" /etc/nginx/conf.d/default.conf \
+    && sed -i "s/auth_basic/# auth_basic/g" /etc/nginx/conf.d/default_ssl.conf;
 fi
 
 /usr/sbin/nginx;

a43103965d7dbdc7ac8023084fe9f06cff78c1e0
aliuosio <oaliu@hotmail.com>
2020-04-04 19:53:38 +0200

    refactored

--- .docker/nginx/docker-entrypoint.sh
+++ .docker/nginx/docker-entrypoint.sh
@@ -21,15 +21,18 @@ authConfig() {
 
 sslConfig() {
     if [ "$1" = "true" ]; then \
-        echo "SSL SETUP START";
-        mkdir -p /etc/nginx/ssl \
-        && mkdir -p /var/cache/ngx_pagespeed \
-        && chown -R $2:$2 /etc/nginx/ssl/ \
-        && chown -R $2:$2 /var/cache/ngx_pagespeed \
-        && chmod 755 -R /var/cache/ngx_pagespeed \
-        && openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/privkey.pem -out /etc/nginx/ssl/fullchain.pem -subj /CN=$3 \
-        && sed -i "s#__default#default_ssl#g" /etc/nginx/nginx.conf; \
-        echo "SSL SETUP END";
+        FILE='/etc/nginx/ssl/privkey.pem';
+        if [ ! -f ${FILE} ]; then \
+            echo "SSL SETUP START";
+            mkdir -p /etc/nginx/ssl \
+            && mkdir -p /var/cache/ngx_pagespeed \
+            && chown -R $2:$2 /etc/nginx/ssl/ \
+            && chown -R $2:$2 /var/cache/ngx_pagespeed \
+            && chmod 755 -R /var/cache/ngx_pagespeed \
+            && openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${FILE} -out /etc/nginx/ssl/fullchain.pem -subj /CN=$3;
+            echo "SSL SETUP END";
+        fi
+        sed -i "s#__default#default_ssl#g" /etc/nginx/nginx.conf;
     else \
         sed -i "s#__default#default#g" /etc/nginx/nginx.conf; \
     fi

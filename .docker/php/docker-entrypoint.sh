8d4c0b278187eb8da5cc56f03382672d59d9d75b
aliuosio <oaliu@hotmail.com>
2020-04-05 21:38:47 +0200

    xdebug config sets on docker-compose up -d

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -6,9 +6,7 @@ set -e
 xdebugConfig() {
     if [[ $1 = "true" ]]; then \
         pecl install -o -f xdebug \
-        && docker-php-ext-enable xdebug \
-        && sed -i "s#xdebug.remote_enable=0#xdebug.remote_enable=1#g" /usr/local/etc/php/conf.d/xdebug.ini \
-        && sed -i "s#xdebug.remote_autostart=0#xdebug.remote_autostart=1#g" /usr/local/etc/php/conf.d/xdebug.ini; \
+        && docker-php-ext-enable xdebug; \
     fi
 }
 

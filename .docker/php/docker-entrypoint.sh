d6d027fbb473dfb93c5fa262a7e91f52f8c7ab66
aliuosio <oaliu@hotmail.com>
2020-04-05 22:53:22 +0200

    moved container config to enrtypoint script

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+
+mainConfig() {
+    ln -snf /usr/share/zoneinfo/1 /etc/localtime \
+    && echo 1 > /etc/timezone \
+    && addgroup -g 1000 -S $2 \
+    && adduser -u 1000 -S -D -G $2 $2 \
+    && chown -R $2:$2 /home/$2 \
+    && echo "export PATH=/home/$2/html/node_modules/.bin:\$PATH" >> /home/$2/.bash_profile \
+    && chmod 775 $3;
+}
+
 # Set xdebug
 xdebugConfig() {
     if [[ $1 = "true" ]]; then \
@@ -10,6 +21,7 @@ xdebugConfig() {
     fi
 }
 
+mainConfig ${TZ} ${USER} ${WORKDIR_SERVER}
 xdebugConfig ${XDEBUG_ENABLE}
 
 php-fpm -F
--- README.md
+++ README.md
@@ -63,7 +63,7 @@ Install [Docker](https://docs.docker.com/engine/installation/linux/docker-ce/ubu
 
 ## PHP Container Usage
     
-    docker exec -it -u $USER mage2_php_7.2 bash
+    docker exec -it -u $USER mage2_php_7.2 bash -l
     
 ## Elasticsearch Usage
 In Magento 2 Backend `stores` -> `Configuration` -> `Catalog` -> `Catalog` -> `Tab: Catalog Search`

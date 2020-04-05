9d2457fffa728cb46e754b2e7c26968739b848b5
aliuosio <oaliu@hotmail.com>
2020-04-05 18:34:51 +0200

    removed unused condition

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -9,10 +9,6 @@ xdebugConfig() {
         && docker-php-ext-enable xdebug \
         && sed -i "s#xdebug.remote_enable=0#xdebug.remote_enable=1#g" /usr/local/etc/php/conf.d/xdebug.ini \
         && sed -i "s#xdebug.remote_autostart=0#xdebug.remote_autostart=1#g" /usr/local/etc/php/conf.d/xdebug.ini; \
-    else
-        pecl uninstall xdebug \
-        && sed -i "s#xdebug.remote_enable=1#xdebug.remote_enable=0#g" /usr/local/etc/php/conf.d/xdebug.ini \
-        && sed -i "s#xdebug.remote_autostart=1#xdebug.remote_autostart=0#g" /usr/local/etc/php/conf.d/xdebug.ini; \
     fi
 }
 

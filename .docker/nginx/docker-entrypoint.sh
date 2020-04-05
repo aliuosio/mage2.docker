0a69eaab5c8187cd23d48f61d08d91dbc3fd10c2
aliuosio <oaliu@hotmail.com>
2020-04-05 17:03:46 +0200

    moved SSL config back to Dockerfile

--- .docker/nginx/docker-entrypoint.sh
+++ .docker/nginx/docker-entrypoint.sh
@@ -19,27 +19,7 @@ authConfig() {
     fi
 }
 
-sslConfig() {
-    if [ "$1" = "true" ]; then \
-        FILE='/etc/nginx/ssl/privkey.pem';
-        if [ ! -f ${FILE} ]; then \
-            echo "SSL SETUP START";
-            mkdir -p /etc/nginx/ssl \
-            && mkdir -p /var/cache/ngx_pagespeed \
-            && chown -R $2:$2 /etc/nginx/ssl/ \
-            && chown -R $2:$2 /var/cache/ngx_pagespeed \
-            && chmod 755 -R /var/cache/ngx_pagespeed \
-            && openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${FILE} -out /etc/nginx/ssl/fullchain.pem -subj /CN=$3;
-            echo "SSL SETUP END";
-        fi
-        sed -i "s#__default#default_ssl#g" /etc/nginx/nginx.conf;
-    else \
-        sed -i "s#__default#default#g" /etc/nginx/nginx.conf; \
-    fi
-}
-
 authConfig ${AUTH_CONFIG} ${AUTH_USER} ${AUTH_PASS}
-sslConfig ${SSL} ${USER} ${SHOP_URI}
 
 /usr/sbin/nginx;
 

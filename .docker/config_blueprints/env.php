58c77cbbafc702932cab6782564bd04505269d23
aliuosio <oaliu@hotmail.com>
2020-04-08 19:42:29 +0200

    refactored

--- .docker/config_blueprints/env.php
+++ .docker/config_blueprints/env.php
@@ -28,7 +28,24 @@ return [
         ]
     ],
     'x-frame-options' => 'SAMEORIGIN',
-    'MAGE_MODE' => 'default',
+    'MAGE_MODE' => 'developer',
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 0,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
     'cache' => [
         'frontend' => [
             'default' => [
@@ -95,23 +112,6 @@ return [
         'config' => [
             'prefix' => NULL
         ]
-    ],
-    'cache_types' => [
-        'config' => 1,
-        'layout' => 1,
-        'block_html' => 1,
-        'collections' => 1,
-        'reflection' => 1,
-        'db_ddl' => 1,
-        'compiled_config' => 1,
-        'eav' => 1,
-        'customer_notification' => 1,
-        'config_integration' => 1,
-        'config_integration_api' => 1,
-        'full_page' => 1,
-        'config_webservice' => 1,
-        'translate' => 1,
-        'vertex' => 1
     ],
     'install' => [
         'date' => 'Mon, 02 Dec 2019 10:20:17 +0000'

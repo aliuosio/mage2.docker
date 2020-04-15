21ac6b7a2c4908942f62a9054088ebf0a6ff7ba4
aliuosio <oaliu@hotmail.com>
2020-04-15 07:53:35 +0200

    moved package installation to Dockerfile

--- .docker/mysql/docker-entrypoint.sh
+++ .docker/mysql/docker-entrypoint.sh
@@ -3,9 +3,7 @@ set -eo pipefail
 shopt -s nullglob
 
 if [[ ! -f '/usr/local/bin/mysqltuner.pl' ]]; then
- apt-get update -y  \
- && apt-get install -y wget  \
- && wget http://mysqltuner.pl/ -O /usr/local/bin/mysqltuner.pl  \
+ wget http://mysqltuner.pl/ -O /usr/local/bin/mysqltuner.pl  \
  && wget https://raw.githubusercontent.com/major/MySQLTuner-perl/master/basic_passwords.txt -O /usr/local/bin/basic_passwords.txt  \
  && wget https://raw.githubusercontent.com/major/MySQLTuner-perl/master/vulnerabilities.csv -O /usr/local/bin/vulnerabilities.csv  \
  && chmod +x /usr/local/bin/mysqltuner.pl  \

f5787c94f42d8d77f72e1a56dd203282acdd8931
aliuosio <oaliu@hotmail.com>
2020-04-17 10:14:02 +0200

    connect set to mariadb socket

--- .docker/config_blueprints/env.php
+++ .docker/config_blueprints/env.php
@@ -10,8 +10,8 @@ return [
         'table_prefix' => '',
         'connection' => [
             'default' => [
-                // 'host' => '/var/run/mysqld/mysqld.sock',
-                'host' => 'db',
+                'host' => '/var/run/mysqld/mysqld.sock',
+                // 'host' => 'db',
                 'dbname' => 'mage2',
                 'username' => 'mage2',
                 'password' => 'mage2',

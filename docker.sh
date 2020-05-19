6cdfe6d71c7d36a3d2d5f0849681d69b012131e3
aliuosio <oaliu@hotmail.com>
2020-05-19 21:51:35 +0200

    added comments

--- docker.sh
+++ docker.sh
@@ -8,6 +8,7 @@ get_latest_release() {
     sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
 
+# docker
 sudo apt-get remove docker docker-engine docker.io containerd runc;
 sudo apt-get update;
 sudo apt-get install \
@@ -19,16 +20,20 @@ sudo apt-get install \
 
 if [[ "$(grep -Ei 'debian' /etc/*release)" ]]; then
     OS="debian";
+    VERSION="buster";
 elif [[ "$(grep -Ei 'ubuntu|mint' /etc/*release)" ]]; then
     OS="ubuntu";
+    VERSION="bionic";
 fi
 
 sudo curl -fsSL https://download.docker.com/linux/${OS}/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$OS $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$OS $VERSION stable"
 sudo apt-get update;
 sudo apt-get install docker-ce docker-ce-cli containerd.io;
 groupadd docker;
 usermod -aG docker ${USER};
 newgrp docker;
+
+# docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/$(get_latest_release docker/compose)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose

912daf48e29c2ec09f26ff08434b8da71ab609d3
aliuosio <oaliu@hotmail.com>
2020-05-19 22:08:24 +0200

    fix

--- docker.sh
+++ docker.sh
@@ -30,8 +30,7 @@ sudo curl -fsSL https://download.docker.com/linux/${OS}/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$OS $VERSION stable"
 sudo apt-get update -y;
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io;
-groupadd docker;
-usermod -aG docker ${USER};
+sudo usermod -aG docker ${USER};
 newgrp docker;
 
 # docker-compose

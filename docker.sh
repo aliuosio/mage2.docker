a47b22045920955218ea1fb01911ce88826bdf35
aliuosio <oaliu@hotmail.com>
2020-05-19 22:00:11 +0200

    added -y option to apt-get commands

--- docker.sh
+++ docker.sh
@@ -9,9 +9,9 @@ get_latest_release() {
 }
 
 # docker
-sudo apt-get remove docker docker-engine docker.io containerd runc;
-sudo apt-get update;
-sudo apt-get install \
+sudo apt-get remove -y docker docker-engine docker.io containerd runc;
+sudo apt-get update -y;
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -28,8 +28,8 @@ fi
 
 sudo curl -fsSL https://download.docker.com/linux/${OS}/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$OS $VERSION stable"
-sudo apt-get update;
-sudo apt-get install docker-ce docker-ce-cli containerd.io;
+sudo apt-get update -y;
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io;
 groupadd docker;
 usermod -aG docker ${USER};
 newgrp docker;

48c1e86c18a96d15dc0724c74fa9d3f06971d98a
aliuosio <oaliu@hotmail.com>
2020-07-29 14:04:09 +0200

    added MariaDB versions

--- install.sh
+++ install.sh
@@ -397,7 +397,7 @@ prompt "rePlaceInEnv" "Absolute path to empty folder(fresh install) or running p
 prompt "rePlaceInEnv" "Domain Name (current: ${SHOPURI})" "SHOPURI"
 specialPrompt "Use Project DB [D]ump, [S]ample Data or [N]one of the above?"
 prompt "rePlaceInEnv" "Which PHP 7 Version? (7.1, 7.2, 7.3, 7.4) (current: ${PHP_VERSION_SET})" "PHP_VERSION_SET"
-prompt "rePlaceInEnv" "Which MariaDB Version? (10.4.10, 10.5.2, 10.5.4) (current: ${MARIADB_VERSION})" "MARIADB_VERSION"
+prompt "rePlaceInEnv" "Which MariaDB Version? (10.4.10, 10.5.2, 10.5, 10.4) (current: ${MARIADB_VERSION})" "MARIADB_VERSION"
 prompt "rePlaceInEnv" "Which Elasticsearch Version? (6.8.11, 7.8.1) (current: ${ELASTICSEARCH_VERSION})" "ELASTICSEARCH_VERSIONs"
 
 MAGE_LATEST="latest"

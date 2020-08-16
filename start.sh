0950a7f93669022ce6380130fee87442e048b9ae
aliuosio <oaliu@hotmail.com>
2020-08-16 16:44:12 +0200

    fix

--- start.sh
+++ start.sh
@@ -2,18 +2,32 @@
 
 set -e
 
-if ! [[ -x "$(command -v docker-compose)" ]]; then
+
+message () {
+  echo "";
+  echo -e "$1"
+  echo "------------------------------------------------------------------------------"
+}
+
+dockerRefresh() {
+    if ! [[ -x "$(command -v docker-compose)" ]]; then
         message 'Error: docker-compose is not installed.' >&2
         exit 1
-fi
+    fi
 
-if [[ $(uname -s) == "Darwin" ]]; then
-    osxExtraPackages
-    rePlaceInEnv "false" "SSL"
-    osxDockerSync
-    message "docker-compose -f docker-compose.osx.yml up -d"
-    docker-compose -f docker-compose.osx.yml up -d
-else
-    message "docker-compose up -d;"
-    docker-compose up -d
-fi
+    if [[ $(uname -s) == "Darwin" ]]; then
+        osxExtraPackages
+        rePlaceInEnv "false" "SSL"
+        osxDockerSync
+        message "docker-compose -f docker-compose.osx.yml up -d"
+        docker-compose -f docker-compose.osx.yml up -d
+    else
+        message "docker-compose up -d;"
+        docker-compose up -d
+    fi
+
+    message "sleep for 1min";
+    sleep 60;
+}
+
+dockerRefresh

6672245fb1f1a845716607a9af56dfb51a1d5f2a
aliuosio <aliu@dev-hh.de>
2020-09-12 20:32:37 +0200

    added new elastic search verions

--- install.sh
+++ install.sh
@@ -435,7 +435,7 @@ prompt "rePlaceInEnv" "Domain Name (current: ${SHOPURI})" "SHOPURI"
 specialPrompt "Use Project DB [d]ump, [s]ample data or [n]one of the above?"
 prompt "rePlaceInEnv" "Which PHP 7 Version? (7.1, 7.2, 7.3, 7.4) (current: ${PHP_VERSION_SET})" "PHP_VERSION_SET"
 prompt "rePlaceInEnv" "Which MySQL Version? (5.7, 8) (current: ${MYSQL_VERSION})" "MYSQL_VERSION"
-prompt "rePlaceInEnv" "Which Elasticsearch Version? (6.8.11, 7.8.1) (current: ${ELASTICSEARCH_VERSION})" "ELASTICSEARCH_VERSION"
+prompt "rePlaceInEnv" "Which Elasticsearch Version? (6.8.11, 7.8.1, 7.9.0, 7.9.1) (current: ${ELASTICSEARCH_VERSION})" "ELASTICSEARCH_VERSION"
 
 MAGE_LATEST="latest"
 read -p "Which Magento 2 Version? (current: ${MAGE_LATEST})" MAGENTO_VERSION

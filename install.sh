c39eb4370a07988619287706d9ffbcb66f5165a7
aliuosio <oaliu@hotmail.com>
2020-10-02 13:22:53 +0200

    made installer magento 2.3.5 compatible

--- install.sh
+++ install.sh
@@ -157,10 +157,7 @@ installMagento() {
     --cache-backend-redis-db=1 \
     --page-cache=redis \
     --page-cache-redis-server=/var/run/redis/redis.sock \
-    --page-cache-redis-db=2 \
-    --search-engine=elasticsearch7 \
-    --elasticsearch-host=elasticsearch \
-    --elasticsearch-port=9200"
+    --page-cache-redis-db=2"
 
   docker exec -it -u "$1" "$3" php -dmemory_limit=-1 -derror_reporting='E_ERROR | E_WARNING | E_PARSE' bin/magento setup:install \
     --db-host=db  \
@@ -190,10 +187,7 @@ installMagento() {
     --cache-backend-redis-db=1 \
     --page-cache=redis \
     --page-cache-redis-server=/var/run/redis/redis.sock \
-    --page-cache-redis-db=2 \
-    --search-engine=elasticsearch7 \
-    --elasticsearch-host=elasticsearch \
-    --elasticsearch-port=9200
+    --page-cache-redis-db=2
 }
 
 setDomainAndCookieName() {

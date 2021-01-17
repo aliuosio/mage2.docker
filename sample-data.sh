1af53d441d757d121168d731b7b043d0a4d4273f
aliuosio <oaliu@hotmail.com>
2021-01-17 17:56:35 +0100

    refactored

--- sample-data.sh
+++ sample-data.sh
@@ -3,14 +3,14 @@
 set -e
 
 sampledata_install() {
-	docker exec -it -u $1 $2 bin/magento sampledata:deploy;
-	docker exec -it -u $1 $2 bin/magento se:up;
-	# docker exec -it -u $1 $2 bin/magento se:di:co;
-	docker exec -it -u $1 $2 bin/magento i:rei;
-	docker exec -it -u $1 $2 bin/magento c:c;
-	# docker exec -it -u $1 $2 bin/magento setup:static-content:deploy -f de_DE en_US;
+	docker exec -u $1 $2 bin/magento sampledata:deploy;
+	docker exec -u $1 $2 bin/magento se:up;
+	# docker exec -u $1 $2 bin/magento se:di:co;
+	docker exec -u $1 $2 bin/magento i:rei;
+	docker exec -u $1 $2 bin/magento c:c;
+	# docker exec -u $1 $2 bin/magento setup:static-content:deploy -f de_DE en_US;
 }
 
 . ${PWD}/.env;
 
-sampledata_install ${USER} ${NAMESPACE}_php_${PHP_VERSION_SET}
+sampledata_install ${USER} ${NAMESPACE}_php

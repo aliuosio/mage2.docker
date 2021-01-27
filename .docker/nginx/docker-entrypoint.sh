c104df3efac0f4d9e4630ffe32131a738c41fedc
aliuosio <oaliu@hotmail.com>
2021-01-27 23:25:50 +0100

    * removed pagespeed container and nginx module * removed SSL references

--- .docker/nginx/docker-entrypoint.sh
+++ .docker/nginx/docker-entrypoint.sh
@@ -23,32 +23,8 @@ mainConfig() {
     sed -i "s#__shopuri#$4#g" /etc/nginx/conf.d/default.conf &&
     sed -i "s#__shopalias#$5#g" /etc/nginx/conf.d/default.conf &&
     sed -i "s#mage2.localhost#$4#g" /etc/nginx/conf.d/default.conf &&
-    sed -i "s#__working_dir#$3#g" /etc/nginx/conf.d/default_ssl.conf &&
-    sed -i "s#htdocs#$3#g" /etc/nginx/conf.d/default_ssl.conf &&
-    sed -i "s#__shopuri#$4#g" /etc/nginx/conf.d/default_ssl.conf &&
-    sed -i "s#mage2.localhost#$4#g" /etc/nginx/conf.d/default_ssl.conf &&
-    sed -i "s#mh.localhost#$4#g" /etc/nginx/conf.d/default_ssl.conf &&
     apk del tzdata &&
     rm -rf /var/cache/apk/*
-}
-
-sslConfig() {
-  if [[ "$1" == "true" ]]; then
-    echo 'SSL Config START'
-    mkdir -p /var/cache/ngx_pagespeed &&
-      mkdir -p /etc/letsencrypt/live/"$3"/ &&
-      chown -R "$2":"$2" /etc/letsencrypt/live/"$3"/ &&
-      chown -R "$2":"$2" /var/cache/ngx_pagespeed &&
-      chmod 755 -R /var/cache/ngx_pagespeed &&
-      openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/letsencrypt/live/"$3"/privkey.pem -out /etc/letsencrypt/live/"$3"/fullchain.pem -subj /CN="$3" &&
-      sed -i "s#__default#default_ssl#g" /etc/nginx/nginx.conf
-    echo 'SSL Config Stop'
-  else
-    echo 'NON SSL Config START'
-    sed -i "s#default_ssl#default#g" /etc/nginx/nginx.conf &&
-      sed -i "s#__default#default#g" /etc/nginx/nginx.conf
-    echo 'NON SSL Config END'
-  fi
 }
 
 setUser() {
@@ -59,9 +35,8 @@ setUser() {
 }
 
 mainConfig "$TZ" "$USER" "$WORKDIR_SERVER" "$SHOPURI" "$SHOPALIAS"
-sslConfig "$SSL" "$USER" "$SHOPURI"
 authConfig "$AUTH_CONFIG" "$AUTH_USER" "$AUTH_PASS"
 setUser "$USER"
 
-/usr/sbin/nginx -g "daemon off; error_log /dev/stderr info;";
+/usr/sbin/nginx -g "daemon off; error_log /dev/stderr info;"
 # /usr/sbin/nginx -g "daemon off; error_log /dev/stderr debug;";

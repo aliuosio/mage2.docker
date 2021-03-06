253a1072d3afdebc25430ac6e478f4dd51d38ff1
aliuosio <oaliu@hotmail.com>
2021-03-06 18:42:54 +0100

    - moved composer install to dockere-compose php entrypoint
    xdebug install moved to php Dockerfile
    
    xdebug config mounted as volume to docker-compose php container

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -17,33 +17,8 @@ phpSettings() {
   sed -i "s#__user#$1#g" /usr/local/etc/php-fpm.d/zz-docker.conf
 }
 
-
 runWaitForIt() {
   wait-for-it.sh db:3306
-}
-
-xdebugConfig() {
-  path="/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini"
-  if "$1" == "true"; then
-    pecl channel-update pecl.php.net
-    pecl install -o -f xdebug
-    docker-php-ext-enable xdebug
-    sed -i "s#xdebug.mode=off#xdebug.mode=debug,develop,trace,coverage#g" /usr/local/etc/php/conf.d/xdebug.ini
-    sed -i "s#xdebug.idekey=docker#xdebug.idekey=$3#g" /usr/local/etc/php/conf.d/xdebug.ini
-    if "$2" == "true"; then
-      sed -i "s#xdebug.profiler_enable=0#xdebug.profiler_enable=1#g" /usr/local/etc/php/conf.d/xdebug.ini
-    fi
-    rm -rf /tmp/pear
-  else
-    pecl uninstall xdebug
-    if test -f "$path"; then
-      rm $path
-    fi
-    sed -i "s#xdebug.mode=debug,develop,trace,coverage#xdebug.mode=off#g" /usr/local/etc/php/conf.d/xdebug.ini
-    if "$2" == "false"; then
-      sed -i "s#xdebug.profiler_enable=1#xdebug.profiler_enable=0#g" /usr/local/etc/php/conf.d/xdebug.ini
-    fi
-  fi
 }
 
 setUser() {
@@ -59,8 +34,14 @@ setUser() {
   fi
 }
 
+composerInstall() {
+  curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer &&
+    chmod +x /usr/local/bin/composer &&
+    composer self-update --1
+}
+
 phpSettings "$USER"
-xdebugConfig "${XDEBUG_ENABLE}" "${XDEBUG_PROFILER}" "${XDEBUG_KEY}"
+composerInstall
 setUser "$USER"
 addPathToBashProfile "$USER"
 runWaitForIt

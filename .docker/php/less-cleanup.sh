579bb43ef48522d6aa26a68b2bd838957c251536
aliuosio <oaliu@hotmail.com>
2021-04-17 00:00:28 +0200

    added script for css/LESS cleanup

---
+++ .docker/php/less-cleanup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+run() {
+  bin/magento se:up;
+  bin/magento dev:source-theme:deploy --locale=de_DE --theme="$1";
+  bin/magento dev:source-theme:deploy --locale=de_DE --theme="$2";
+  bin/magento s:s:d -f de_DE;
+  bin/magento ca:cl;
+}
+
+run "$1" "$2"
+
+
+
+

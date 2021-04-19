75a5d58d1e99033a97cb64eedd5592badac7919d
aliuosio <oaliu@hotmail.com>
2021-04-19 14:36:52 +0200

    added gulp css command added gulp watch command

--- .docker/php/less-cleanup.sh
+++ .docker/php/less-cleanup.sh
@@ -3,15 +3,13 @@
 set -e
 
 run() {
-  bin/magento se:up;
-  bin/magento dev:source-theme:deploy --locale=de_DE --theme="$1";
-  bin/magento dev:source-theme:deploy --locale=de_DE --theme="$2";
-  bin/magento s:s:d -f de_DE;
-  bin/magento ca:cl;
+  bin/magento se:up
+  bin/magento dev:source-theme:deploy --locale=de_DE --theme="$1"
+  bin/magento dev:source-theme:deploy --locale=de_DE --theme="$2"
+  bin/magento s:s:d -f de_DE
+  bin/magento ca:cl
+  gulp css
+  gulp watch
 }
 
 run "$1" "$2"
-
-
-
-

f055283727bf017f4ef3685f5a1cdf2c20c07820
aliuosio <oaliu@hotmail.com>
2021-04-20 19:26:38 +0200

    added yarn install

--- .docker/php/less-cleanup.sh
+++ .docker/php/less-cleanup.sh
@@ -2,17 +2,41 @@
 
 set -e
 
-run() {
-for var in "$@"
-do
-  bin/magento dev:source-theme:deploy --locale=de_DE --theme="$var"
-done
+createSource() {
+  for var in "$@"; do
+    bin/magento dev:source-theme:deploy --locale=de_DE --theme="$var"
+  done
+}
 
+InstallPackages() {
+  yarn install
+  yarn upgrade
+}
 
-  bin/magento s:s:d -f de_DE
-  bin/magento ca:cl
+gulpCommands() {
   gulp css
   gulp watch
+}
+
+createStaticFiles() {
+  bin/magento s:s:d -f de_DE en_US
+}
+
+cacheClear() {
+  bin/magento ca:cl
+}
+
+setupUpgrade() {
+  bin/magento se:up
+}
+
+run() {
+  setupUpgrade
+  createSource "$@"
+  createStaticFiles
+  cacheClear
+  InstallPackages
+  gulpCommands
 }
 
 run "$@"

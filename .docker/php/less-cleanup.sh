02e183e63fa5a6f12fc0e7ba38856bc87823a7d7
aliuosio <oaliu@hotmail.com>
2021-04-20 15:40:08 +0200

    loop over themes sent to generate LESS Source tree

--- .docker/php/less-cleanup.sh
+++ .docker/php/less-cleanup.sh
@@ -3,13 +3,16 @@
 set -e
 
 run() {
-  # bin/magento se:up
-  bin/magento dev:source-theme:deploy --locale=de_DE --theme="$1"
-  bin/magento dev:source-theme:deploy --locale=de_DE --theme="$2"
+for var in "$@"
+do
+  bin/magento dev:source-theme:deploy --locale=de_DE --theme="$var"
+done
+
+
   bin/magento s:s:d -f de_DE
   bin/magento ca:cl
   gulp css
   gulp watch
 }
 
-run "$1" "$2"
+run "$@"

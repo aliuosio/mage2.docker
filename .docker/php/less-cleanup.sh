b5c795a2d5c8aee44f5507af8e826eeddcbad4e7
aliuosio <oaliu@hotmail.com>
2021-05-05 06:23:51 +0200

    outputs used commands in script

--- .docker/php/less-cleanup.sh
+++ .docker/php/less-cleanup.sh
@@ -2,39 +2,56 @@
 
 set -e
 
+message() {
+  echo ""
+  echo -e "$1"
+  seq ${#1} | awk '{printf "-"}'
+  echo ""
+}
+
+setupUpgrade() {
+  message 'bin/magento se:up'
+  bin/magento se:up
+}
+
 createSource() {
   for var in "$@"; do
+    message "bin/magento dev:source-theme:deploy --locale=de_DE --theme=""$var""";
     bin/magento dev:source-theme:deploy --locale=de_DE --theme="$var"
   done
 }
 
+cacheClear() {
+  message 'bin/magento ca:cl'
+  bin/magento ca:cl
+}
+
+createStaticFiles() {
+  message 'bin/magento s:s:d -f en_US de_DE'
+  bin/magento s:s:d -f en_US de_DE
+}
+
 installPackages() {
+  message 'yarn install'
   yarn install
+
+  message 'yarn upgrade';
   yarn upgrade
 }
 
 gulpCommands() {
+  message 'gulp css'
   gulp css
+
+  message 'gulp watch'
   gulp watch
-}
-
-createStaticFiles() {
-  bin/magento s:s:d -f de_DE en_US
-}
-
-cacheClear() {
-  bin/magento ca:cl
-}
-
-setupUpgrade() {
-  bin/magento se:up
 }
 
 run() {
   setupUpgrade
   createSource "$@"
-  createStaticFiles
   cacheClear
+  createStaticFiles
   installPackages
   gulpCommands
 }

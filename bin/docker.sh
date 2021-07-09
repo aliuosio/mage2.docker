d469a5c79426efb1672c6f1c47382544c3d43b7a
Osio Aliu <oaliu@hotmail.com>
2021-07-09 15:15:56 +0200

    restored

---
+++ bin/docker.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+# docker
+sudo apt-get remove docker docker-engine docker.io containerd runc || true;
+sudo apt-get update -y;
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common;
+
+if [[ "$(grep -Ei 'debian' /etc/*release)" ]]; then
+    OS="debian";
+    VERSION="buster";
+elif [[ "$(grep -Ei 'ubuntu|mint' /etc/*release)" ]]; then
+    OS="ubuntu";
+    VERSION="bionic";
+fi
+
+sudo curl -fsSL https://download.docker.com/linux/${OS}/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$OS $VERSION stable"
+sudo apt-get update -y;
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io;
+
+# docker-compose
+echo "sudo curl -L "https://github.com/docker/compose/releases/download/$(get_latest_release docker/compose)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose";
+sudo curl -L "https://github.com/docker/compose/releases/download/$(get_latest_release docker/compose)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+echo "sudo chmod +x /usr/local/bin/docker-compose";
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo usermod -aG docker ${USER};
+newgrp docker;

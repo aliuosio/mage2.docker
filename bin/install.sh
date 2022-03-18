14b97fb51a45a956a17c254e6631f080bb7f14b0
Osiozekhai Aliu <aliu@dev-hh.de>
2022-03-18 19:23:04 +0100

    Revert "added magerun"
    This reverts commit 76a00ea17c4b102c08f511e8c17d2fbc25c63949.

--- bin/install.sh
+++ bin/install.sh
@@ -47,7 +47,6 @@ sampleDataInstallMustInstall
 magentoRefresh
 setMagentoPermissions
 setPermissionsContainer
-magerunInstall
 endAll=$(date +%s)
 message "Setup Time: $((endAll - startAll)) Sec"
 showSuccess "$SHOPURI" "$DUMP"

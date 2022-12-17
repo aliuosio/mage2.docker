7615c4ef5aed8dfa663f925ff98c587f6a8cf1a8
Osiozekhai <oaliu@hotmail.com>
2022-12-17 11:30:27 +0100

    updated redis dbs

--- bin/includes/functions.sh
+++ bin/includes/functions.sh
@@ -362,9 +362,9 @@ magentoInstall() {
   --admin-user=$ADMIN_USER --admin-password=$ADMIN_PASS \
   --search-engine=elasticsearch7 --elasticsearch-host=elasticsearch --elasticsearch-port=9200 \
   --amqp-host=rabbitmq --amqp-ssl=false --amqp-port=5672 --amqp-user=admin --amqp-password=admin2017 --amqp-virtualhost='/' \
-  --page-cache=redis --page-cache-redis-server=redis --page-cache-redis-db=1 \
+  --page-cache=redis --page-cache-redis-server=redis --page-cache-redis-db=0 \
   --cache-backend=redis --cache-backend-redis-server=redis --cache-backend-redis-db=1 \
-  --session-save=redis --session-save-redis-host=redis --session-save-redis-persistent-id=sess-db2 --session-save-redis-db=1 \
+  --session-save=redis --session-save-redis-host=redis --session-save-redis-persistent-id=sess-db2 --session-save-redis-db=2 \
   --timezone=Europe/Berlin --currency=EUR --language=de_DE \
   --cleanup-database"
 
